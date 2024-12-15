49c7d6b74e793d72c9f18f4b019752166b684725
hughes <hughes036@gmail.com>
2024-12-15 17:01:41 -0500

    refactoring

--- range/range.py
+++ range/range.py
@@ -14,27 +14,16 @@ class Range:
     def add(self, start, end):
         self.ranges.append((start, end))
     
-    # add:
-    # (0, 100)
-    # remove:
-    # (25, 75)
-    # leaves:
-    # (0, 25)
-    # (75, 100)
-    # remove:
-    # (1, 100)
     def remove(self, start, end):
         ranges_to_update = [r for r in self.ranges if startInRange(start, r) or endInRange(end, r) or (start <= r[0] and end >= r[1])]
         for r in ranges_to_update:
             self.ranges.remove(r)
-            # does range being removed include all of r?
-            if start <= r[0] and end >= r[1]:
-                # remove the range and add nothing
-                continue
-            if start > r[0]:
-                self.add(r[0], start - 1)
-            if end < r[1]:
-                self.add(end + 1, r[1])
+            # does range being removed overlap with r?
+            if start > r[0] or end < r[1]:
+                if start > r[0]:
+                    self.add(r[0], start - 1)
+                if end < r[1]:
+                    self.add(end + 1, r[1])
 
     def query(self, value) -> bool:
         ranges_including_value = [r for r in self.ranges if value >= r[0] and value <= r[1]]
@@ -48,8 +37,10 @@ def main():
     print(r.ranges)
     r.remove(80, 200)
     print(r.ranges)
-    r.remove(1, 100)
-    print(r.ranges) 
+    r.remove(-100, -1) # no-op
+    print(r.ranges)  
+    r.remove(101, 200) # no-op
+    print(r.ranges)
     r.remove(0, 100)
     print(r.ranges) 
 
